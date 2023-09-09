722c5258bd00dffafbaa987cb60a68a965232f7c
Heiko Marschner <heiko.marschner@gmail.com>
2023-09-09 17:09:24 +0200

    Make sure has file exists

--- python/push_over/hashes.py
+++ python/push_over/hashes.py
@@ -1,18 +1,21 @@
 """ Hash File Support """
 
 import logging
+import os
 
 
 class Hashes:
-    """ Hash File Support """
+    """Hash File Support"""
 
     def __init__(self, filename: str, config_dir: str) -> None:
-        if filename.startswith('/'):
+        if filename.startswith("/"):
             self.filename = filename
         else:
-            self.filename = config_dir + '/' + filename
+            self.filename = config_dir + "/" + filename
+        with open(file=self.filename, mode="a", encoding="utf-8"):
+            pass
         logging.debug("Using hash file %s", self.filename)
-        self.separator = ' - '
+        self.separator = " - "
         self.hashes = {}
         with open(self.filename, "r", encoding="utf-8") as file:
             for line in file.readlines():
@@ -20,11 +23,11 @@ class Hashes:
                 self.hashes.update({parts[0]: parts[1]})
 
     def already_known(self, key):
-        """ Hash already known """
+        """Hash already known"""
         return key in self.hashes
 
     def add(self, key, value):
-        """ Add hash """
+        """Add hash"""
         with open(self.filename, "a", encoding="utf-8") as file:
-            file.write(key + self.separator + value + '\n')
+            file.write(key + self.separator + value + "\n")
             self.hashes.update({key: value})
